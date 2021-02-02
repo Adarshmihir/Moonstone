482560053b33a7915164023650135b8e25b3aa6a
Corentin_Jn <corentin.jean98@gmail.com>
2021-02-02 00:03:18 -0500

    HealthGlobe sprites added  + control test

---
+++ Assets/Scripts/HealthGlobeControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthGlobeControl : MonoBehaviour
+{
+    public Slider healthSlider;
+    public float regenSpeed;
+    public bool regenering;
+
+    void Start()
+    {
+        healthSlider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(regenering)
+        {
+            if(healthSlider.value < 1)
+            {
+                healthSlider.value += (regenSpeed * Time.deltaTime);
+            }
+            if(healthSlider.value > 1)
+            {
+                healthSlider.value = 1;
+                regenering = false;
+            }
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            healthSlider.value = healthSlider.value - 0.1f;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            regenering = true;
+        }
+    }
+}
