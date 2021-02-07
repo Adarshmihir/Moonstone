3eafb9f3d4aacd4c1c7f5e6fcb696977f24515ea
AlanGuivarch <alan.guivarch.98860@gmail.com>
2021-02-06 20:36:06 -0500

    Plant_Interaction finished
    Creat Plant.cs and plantPickUp.cs to interact with different plants on the scene. PlantCounter is used for debug

---
+++ Assets/Scripts/Plant/plantPickUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plantPickUp : Interactable
+{
+    public Plant plant;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    private void PickUp()
+    {
+        Debug.Log("Picking up " + plant.name + " !");
+        GameObject player = GameObject.Find("Player");
+        switch (plant.name)
+        {
+            case ("Plant1"):
+            {
+                player.GetComponent<PlantCounter>().addPlant1();
+                break;
+            }
+            case ("Plant2"):
+            {
+                    player.GetComponent<PlantCounter>().addPlant2();
+                    break;
+            }
+            case ("Plant3"):
+            {
+                    player.GetComponent<PlantCounter>().addPlant3();
+                    break;
+            }
+        }
+        player.GetComponent<PlantCounter>().showCount();
+        Destroy(transform.gameObject);
+    }
+}
