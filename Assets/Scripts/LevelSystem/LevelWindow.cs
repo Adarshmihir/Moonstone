91cd1a43b45be110f3a238f02a8069a08f3d758a
Corentin_Jn <corentin.jean98@gmail.com>
2021-02-15 11:07:54 -0500

    Add UI basic leveling system feature

---
+++ Assets/Scripts/LevelSystem/LevelWindow.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelWindow : MonoBehaviour
+{
+    private Text levelText;
+    private Image xpBarImage;
+    private XpSystem levelSystem;
+    private void Awake()
+    {
+        levelText = transform.Find("levelText").GetComponent<Text>();
+        xpBarImage = transform.Find("xpBar").Find("bar").GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("XP = " + levelSystem.GetXPAmount());
+            levelSystem.AddExperience(10);
+            Debug.Log("XP + 10 ");
+            Debug.Log("XP = " + levelSystem.GetXPAmount());
+            Debug.Log("XP next level = " + levelSystem.GetXPForLevel(levelSystem.GetLevelNumber()+1));
+        }
+    }
+    private void SetExperienceBarSize(float xpNormalized)
+    {
+        xpBarImage.fillAmount = xpNormalized;
+    }
+
+    private void SetLevelNumber(int levelNumber)
+    {
+        levelText.text = "LEVEL" + (levelNumber + 1);
+    }
+
+    public void SetLevelSystem(XpSystem levelSystem)
+    {
+        this.levelSystem = levelSystem;
+
+        SetLevelNumber(levelSystem.GetLevelNumber());
+
+        SetExperienceBarSize(levelSystem.GetExperienceNormalized());
+
+        levelSystem.OnExperienceChanged += LevelSystem_OnExperienceChanged;
+        levelSystem.OnLevelChanged += LevelSystem_OnLevelChanged;
+    }
+
+    private void LevelSystem_OnExperienceChanged(object sender, System.EventArgs e)
+    {
+        SetExperienceBarSize(levelSystem.GetExperienceNormalized());
+    }
+
+    private void LevelSystem_OnLevelChanged(object sender, System.EventArgs e)
+    {
+        SetLevelNumber(levelSystem.GetLevelNumber());
+    }
+}
