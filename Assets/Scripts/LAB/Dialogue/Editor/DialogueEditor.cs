18005540b2b1090c9686b945fd107dd79d1a060a
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2021-02-17 04:48:58 +0100

    Opening dialogue scriptable objet in editor

--- Assets/Scripts/LAB/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/LAB/Dialogue/Editor/DialogueEditor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace Dialogue.Editor
@@ -11,6 +12,16 @@ namespace Dialogue.Editor
         public static void ShowDialogueEditor()
         {
             GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+        }
+
+        [OnOpenAsset(1)]
+        public static bool OnOpenDialogue(int instanceID, int line)
+        {
+            var dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            if (dialogue == null) return false;
+
+            ShowDialogueEditor();
+            return true;
         }
     }
 }
