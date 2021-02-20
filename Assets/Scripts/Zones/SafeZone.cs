a94dbd61839cf244f533ed03d12869419eecce82
Alexandre <sacdeneu@gmail.com>
2021-02-20 01:00:27 +0100

    Zone script and safezone script added + implementation

---
+++ Assets/Scripts/Zones/SafeZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Control;
+using UnityEngine;
+
+public class SafeZone : Zone
+{
+    
+    protected override void UseTriggerZone(Collider col)
+    {
+        base.UseTriggerZone(col);
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            col.gameObject.GetComponent<AIController>().returnToInitialPosition();
+        }
+    }
+
+}
