2eb22649e2afa50bab3653dd2c0a13353db49f19
Corentin_Jn <corentin.jean98@gmail.com>
2021-02-24 18:57:38 -0500

    Add Energy Globe UI + functions

---
+++ Assets/Scripts/EnergyGlobeControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyGlobeControl : MonoBehaviour
+{
+    public Slider energySlider;
+
+    private int maxEnergy = 100;
+
+    void Start()
+    {
+        energySlider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            UseEnergy(20);
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            RestoreEnergy(10);
+        }
+    }
+
+    public void UseEnergy(float cost)
+    {
+        if (energySlider.value*maxEnergy < cost)
+        {
+            //Cannot launch a spell
+            Debug.Log("Pas assez de mana");
+        }
+        else
+        {
+            energySlider.value -= cost / maxEnergy;
+        }
+    }
+
+    public void RestoreEnergy(float regen)
+    {
+        if (energySlider.value + regen/maxEnergy > 1)
+        {
+            energySlider.value = 1;
+        }
+        else
+        {
+            energySlider.value += regen / maxEnergy;
+        }
+    }
+}
