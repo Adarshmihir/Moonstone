6cf176af6c7e89bba037864894a5186d1ee2cdbf
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2021-02-24 03:33:55 -0500

    Dialogue UI

---
+++ Assets/Scripts/LAB/Dialogue/DialogueUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+namespace Dialogue
+{
+	public class DialogueUI : MonoBehaviour
+	{
+		[SerializeField] private TextMeshProUGUI questText;
+
+		private PlayerDialogue _playerDialogue;
+
+		// Use this for initialization
+		private void Start()
+		{
+			_playerDialogue = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDialogue>();
+
+		}
+
+		// Update is called once per frame
+		private void Update()
+		{
+
+		}
+	}
+}
---
+++ Assets/Scripts/LAB/Dialogue/PlayerDialogue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dialogue
+{
+    public class PlayerDialogue : MonoBehaviour
+    {
+        [SerializeField] private Dialogue dialogue;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+
+        public string GetText()
+		{
+            if (dialogue == null) return "";
+
+            return "";
+		}
+    }
+}
