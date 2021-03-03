522ee795358efde253fe77c0d0426a16f1bd0a29
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2021-03-03 02:42:23 -0500

    Quest menu button

---
+++ Assets/Scripts/LAB/UI/Quests/HideQuestUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideQuestUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Toggle()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
