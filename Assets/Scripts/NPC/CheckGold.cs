cec27cb07b8c11b13d2fe588bc35125729d2d1d3
Corentin_Jn <corentin.jean98@gmail.com>
2021-03-15 23:46:01 -0400

    Add tuto plant et fix somes weird dialogues

---
+++ Assets/Scripts/NPC/CheckGold.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quests;
+
+public class CheckGold : MonoBehaviour
+{
+    public int goldNeeded;
+    public Quest quest;
+    public string idGoal;
+    private GameObject gm;
+
+    private void Start()
+    {
+        gm = GameObject.Find("GameManager");
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        var player = GameManager.Instance.player;
+        var questList = player.GetComponent<QuestManager>();
+
+        var evaluatedQuest = questList.Evaluate("HasQuest", quest.Name);
+        if (evaluatedQuest != null && gm.GetComponent<Inventory>().gold >= goldNeeded)
+        {
+            questList.CompleteGoal(questList.GetQuestByName(quest.Name), idGoal);
+        }
+    }
+}
