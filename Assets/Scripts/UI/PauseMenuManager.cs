3449bd5044dc027487b0a189cbc9532d8919aab7
Alexandre <sacdeneu@gmail.com>
2021-03-28 20:12:21 +0200

    Graphics quality and fullscreen parameter auto-save + hotfix UI
    Pause menu UI can now only be shown when parameters and keybinds windows are closed

--- Assets/Scripts/UI/PauseMenuManager.cs
+++ Assets/Scripts/UI/PauseMenuManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -9,14 +10,40 @@ public class PauseMenuManager : MonoBehaviour
 
     public GameObject pauseMenuUI;
 
+    public GameObject ParamsMenuUI;
+
+    public GameObject BindingsMenuUI;
+
+    private bool menuActive;
     // Update is called once per frame
+
+    private void Start()
+    {
+        menuActive = false;
+    }
+
     void Update()
     {
-        if(Input.GetButtonDown("MainMenu"))
+        if (!Input.GetButtonDown("MainMenu")) return;
+        if (!pauseMenuUI.activeSelf)
         {
-            OpenCloseMainMenu();
-            Debug.Log("Echap pressed");
-        }  
+            if (BindingsMenuUI.activeSelf || ParamsMenuUI.activeSelf)
+            {
+                menuActive = true;
+            }
+            else
+            {
+                menuActive = false;
+            }
+        }
+        else
+        {
+            menuActive = false;
+        }
+
+        if (menuActive) return;
+        OpenCloseMainMenu();
+        Debug.Log("Echap pressed");
     }
 
     public void OpenCloseMainMenu()
--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
+using Toggle = UnityEngine.UI.Toggle;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -11,10 +15,18 @@ public class SettingsMenu : MonoBehaviour
     private static readonly string FirstPlay = "FirstPlay";
     private static readonly string MusicPref = "MusicPref";
     private static readonly string SoundEffectsPref = "SoundEffectsPref";
+    private static readonly string FullScreenPref = "FullScreenPref";
+    private static readonly string GraphicsPref = "GraphicsPref";
+    private static readonly string ResolutionPref = "ResolutionPref";
     private int firstPlayInt;
+    public Toggle toggleFullscreen;
     public Dropdown resolutionDropdown;
     public Slider musicSlider, soundEffectsSlider;
+    public Text label_graphics;
+    public Dropdown dropwdown_graphics;
     private float musicFloat, soundEffectsFloat;
+    private int is_FullScreenOn;
+    private int qualityIndex;
     public AudioMixer mixer;
     private void Start()
     {
@@ -41,11 +53,15 @@ public class SettingsMenu : MonoBehaviour
         resolutionDropdown.value = currentResIndex;
         resolutionDropdown.RefreshShownValue();
         
-        //MUSIC
+        //FIRST PLAY
         firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
         if (firstPlayInt == 0)
         {
             //initial values
+            is_FullScreenOn = 0;
+            qualityIndex = 2;
+            label_graphics.text = dropwdown_graphics.options.ElementAt(qualityIndex).text;
+            toggleFullscreen.isOn = false;
             musicFloat = .125f;
             soundEffectsFloat = .75f;
             musicSlider.value = musicFloat;
@@ -53,10 +69,27 @@ public class SettingsMenu : MonoBehaviour
             PlayerPrefs.SetFloat(MusicPref, musicFloat);
             PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectsFloat);
             PlayerPrefs.SetInt(FirstPlay, -1);
+            PlayerPrefs.SetInt(FullScreenPref, is_FullScreenOn);
+            PlayerPrefs.SetInt(GraphicsPref, qualityIndex);
         }
-        else
+        else //AFTER FIRST PLAY 
         {
             //setting parameters based on prefs
+            is_FullScreenOn = PlayerPrefs.GetInt(FullScreenPref);
+            if (is_FullScreenOn==0)
+            {
+                toggleFullscreen.isOn = false;
+                SetFullScreen(false);
+            }
+            else
+            {
+                toggleFullscreen.isOn = true;
+                SetFullScreen(true);
+            }
+            qualityIndex = PlayerPrefs.GetInt(GraphicsPref);
+            dropwdown_graphics.value = qualityIndex;
+            label_graphics.text = dropwdown_graphics.options.ElementAt(qualityIndex).text;
+            SetQuality(qualityIndex);
             musicFloat = PlayerPrefs.GetFloat(MusicPref);
             musicSlider.value = musicFloat;
             SetMusicVolumeLevel(musicFloat);
@@ -68,12 +101,21 @@ public class SettingsMenu : MonoBehaviour
     }
     public void SetQuality(int qualityIndex)
     {
+        PlayerPrefs.SetInt(GraphicsPref, qualityIndex);
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
     public void SetFullScreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+        if (isFullscreen)
+        {
+            PlayerPrefs.SetInt(FullScreenPref,1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt(FullScreenPref,0);
+        }
     }
 
     public void SetResolution(int resolutionIndex)
@@ -87,10 +129,17 @@ public class SettingsMenu : MonoBehaviour
         PlayerPrefs.SetFloat(MusicPref, musicSlider.value);
         PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectsSlider.value);
     }
+    
+    public void SaveGraphicsSettings()
+    {
+        PlayerPrefs.SetInt(FullScreenPref, is_FullScreenOn);
+        PlayerPrefs.SetInt(GraphicsPref, qualityIndex);
+    }
 
     public void SaveSettings()
     {
         SaveSoundSettings();
+        SaveGraphicsSettings();
     }
     
     private void OnApplicationFocus(bool inFocus)
@@ -99,7 +148,7 @@ public class SettingsMenu : MonoBehaviour
         {
             if (gameObject.activeSelf == true)
             {
-                SaveSoundSettings(); //save values if no focus (reduce app or go on another window)
+                SaveSettings(); //save values if no focus (reduce app or go on another window)
             }
         }
     }
