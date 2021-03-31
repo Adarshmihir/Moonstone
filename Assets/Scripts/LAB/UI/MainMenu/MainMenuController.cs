b08baff6a7e22120405f91bb441416ea28daf9c7
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2021-03-31 18:14:11 -0400

    Home screen

---
+++ Assets/Scripts/LAB/UI/MainMenu/MainMenuController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button newGameButton;
+        [SerializeField] private Button settingsButton;
+        [SerializeField] private Button quitButton;
+
+        [SerializeField] private Animator playerAnimator;
+        [SerializeField] private Animator sceneAnimator;
+
+        [SerializeField] private float playerToFadeTimer = 1f;
+        [SerializeField] private float fadeToSkipTimer = 1f;
+    
+        // Start is called before the first frame update
+        private void Start()
+        {
+            playButton.onClick.AddListener(PlayerStandUp);
+            newGameButton.onClick.AddListener(PlayerStandUp);
+            settingsButton.onClick.AddListener(OnSettingsPressed);
+            quitButton.onClick.AddListener(OnQuitPressed);
+        }
+
+        private void PlayerStandUp()
+        {
+            StartCoroutine(StandUp());
+        }
+
+        private static void OnSettingsPressed()
+        {
+        
+        }
+
+        private static void OnQuitPressed()
+        {
+            Application.Quit();
+        }
+
+        private IEnumerator StandUp()
+        {
+            playerAnimator.SetTrigger("standUp");
+            
+            yield return new WaitForSeconds(playerToFadeTimer);
+
+            StartCoroutine(LoadScene());
+        }
+
+        private IEnumerator LoadScene()
+        {
+            sceneAnimator.SetTrigger("startFade");
+            
+            yield return new WaitForSeconds(fadeToSkipTimer);
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
